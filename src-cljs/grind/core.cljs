f0608297e24d987731f31556099b91f03f0bbe29
Florian Hassanen <florian.hassanen@gmail.com>
2014-04-26 15:11:27 +0200

    initial canvas code

--- src-cljs/grind/core.cljs
+++ src-cljs/grind/core.cljs
@@ -1,7 +1,16 @@
 (ns grind.core
   (:require [clojure.browser.repl :as repl]))
 
+(defn ctx []
+  (.. js/document (getElementById "grind") (getContext "2d")))
+
 (defn ^:export greet [n]
   (str "Hello " n))
 
+
+
 (repl/connect "http://localhost:9000/repl")
+
+(let [c (ctx)]
+  (set! (.-fillStyle c) "rgb(200,0,0)")
+  (.fillRect c 10 10 100 100))

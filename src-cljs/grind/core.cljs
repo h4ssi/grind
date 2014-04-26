ec304d4f8f203ab424f4e2c403f0b867f57911ea
Florian Hassanen <florian.hassanen@gmail.com>
2014-04-26 15:46:02 +0200

    basic animation

--- src-cljs/grind/core.cljs
+++ src-cljs/grind/core.cljs
@@ -11,6 +11,15 @@
 
 (repl/connect "http://localhost:9000/repl")
 
+(defn draw [x]
 (let [c (ctx)]
+  (.clearRect c 0 0 200 200)
   (set! (.-fillStyle c) "rgb(200,0,0)")
-  (.fillRect c 10 10 100 100))
+  (.fillRect c (+ 10 x) 10 100 100)))
+
+(defn frame [t]
+  ;(.log js/console t)
+  (draw (* 10 (.sin js/Math (* 0.001 t))))
+  (.requestAnimationFrame js/window frame))
+
+(.requestAnimationFrame js/window frame)

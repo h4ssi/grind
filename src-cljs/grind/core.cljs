10b425c45aa29b520a1309c06fa6c978992a29bc
Florian Hassanen <florian.hassanen@gmail.com>
2014-04-26 17:03:46 +0200

    initial game loop

--- src-cljs/grind/core.cljs
+++ src-cljs/grind/core.cljs
@@ -1,25 +1,47 @@
 (ns grind.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [cljs.core.async :refer [chan sliding-buffer >! <!]])
+  (:require-macros [cljs.core.async.macros :refer [go alt!]]))
 
 (defn ctx []
   (.. js/document (getElementById "grind") (getContext "2d")))
 
-(defn ^:export greet [n]
-  (str "Hello " n))
-
-
-
-(repl/connect "http://localhost:9000/repl")
+(def drawn (chan (sliding-buffer 1))) 
+(def pressed (chan))
+(def released (chan))
 
 (defn draw [x]
-(let [c (ctx)]
-  (.clearRect c 0 0 200 200)
-  (set! (.-fillStyle c) "rgb(200,0,0)")
-  (.fillRect c (+ 10 x) 10 100 100)))
+  (let [c (ctx)]
+    (.clearRect c 0 0 200 200)
+    (set! (.-fillStyle c) "rgb(200,0,0)")
+    (.fillRect c (+ 10 x) 10 100 100)))
 
-(defn frame [t]
+(defn frame [t] ; t in millis
   ;(.log js/console t)
-  (draw (* 10 (.sin js/Math (* 0.001 t))))
-  (.requestAnimationFrame js/window frame))
 
-(.requestAnimationFrame js/window frame)
+  (draw (* 10 (.sin js/Math (* 0.001 t))))
+  (go (>! drawn t)))
+
+(def initial-state {:x 100 :y 100})
+
+(defn game-loop []
+  (go (loop [state initial-state]
+        (alt! [drawn] (.requestAnimationFrame js/window frame)
+              [pressed] nil
+              [released] nil)
+        (recur state))))
+
+(defn ^:export start []
+  (repl/connect "http://localhost:9000/repl")
+  (game-loop)
+  (go (>! drawn 0)))
+
+(defn ^:export keydown [e]
+  (.preventDefault e)
+  ;(.log js/console e)
+  (go (>! pressed e)))
+
+(defn ^:export keyup [e]
+  (.preventDefault e)
+  ;(.log js/console e)
+  (go (>! released e)))
